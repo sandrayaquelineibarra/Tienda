e299575b7c6eda24ba65b56185fbae14707a142e
sandrayaquelineibarra <96554849+sandrayaquelineibarra@users.noreply.github.com>
2023-10-10 23:52:49 -0300

    Add files via upload

---
+++ tienda.sql
@@ -0,0 +1,113 @@
+DROP DATABASE IF EXISTS jardineria;
+CREATE DATABASE jardineria CHARACTER SET utf8mb4;
+USE jardineria;
+
+CREATE TABLE oficina (
+  codigo_oficina VARCHAR(10) NOT NULL,
+  ciudad VARCHAR(30) NOT NULL,
+  pais VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_oficina)
+);
+
+CREATE TABLE empleado (
+  codigo_empleado INTEGER NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) DEFAULT NULL,
+  extension VARCHAR(10) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  codigo_oficina VARCHAR(10) NOT NULL,
+  codigo_jefe INTEGER DEFAULT NULL,
+  puesto VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_empleado),
+  FOREIGN KEY (codigo_oficina) REFERENCES oficina (codigo_oficina),
+  FOREIGN KEY (codigo_jefe) REFERENCES empleado (codigo_empleado)
+);
+
+CREATE TABLE gama_producto (
+  gama VARCHAR(50) NOT NULL,
+  descripcion_texto TEXT,
+  descripcion_html TEXT,
+  imagen VARCHAR(256),
+  PRIMARY KEY (gama)
+);
+
+CREATE TABLE cliente (
+  codigo_cliente INTEGER NOT NULL,
+  nombre_cliente VARCHAR(50) NOT NULL,
+  nombre_contacto VARCHAR(30) DEFAULT NULL,
+  apellido_contacto VARCHAR(30) DEFAULT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  fax VARCHAR(15) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  ciudad VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  pais VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) DEFAULT NULL,
+  codigo_empleado_rep_ventas INTEGER DEFAULT NULL,
+  limite_credito NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_cliente),
+  FOREIGN KEY (codigo_empleado_rep_ventas) REFERENCES empleado (codigo_empleado)
+);
+
+CREATE TABLE pedido (
+  codigo_pedido INTEGER NOT NULL,
+  fecha_pedido date NOT NULL,
+  fecha_esperada date NOT NULL,
+  fecha_entrega date DEFAULT NULL,
+  estado VARCHAR(15) NOT NULL,
+  comentarios TEXT,
+  codigo_cliente INTEGER NOT NULL,
+  PRIMARY KEY (codigo_pedido),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
+
+CREATE TABLE producto (
+  codigo_producto VARCHAR(15) NOT NULL,
+  nombre VARCHAR(70) NOT NULL,
+  gama VARCHAR(50) NOT NULL,
+  dimensiones VARCHAR(25) NULL,
+  proveedor VARCHAR(50) DEFAULT NULL,
+  descripcion text NULL,
+  cantidad_en_stock SMALLINT NOT NULL,
+  precio_venta NUMERIC(15,2) NOT NULL,
+  precio_proveedor NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_producto),
+  FOREIGN KEY (gama) REFERENCES gama_producto (gama)
+);
+
+CREATE TABLE detalle_pedido (
+  codigo_pedido INTEGER NOT NULL,
+  codigo_producto VARCHAR(15) NOT NULL,
+  cantidad INTEGER NOT NULL,
+  precio_unidad NUMERIC(15,2) NOT NULL,
+  numero_linea SMALLINT NOT NULL,
+  PRIMARY KEY (codigo_pedido, codigo_producto),
+  FOREIGN KEY (codigo_pedido) REFERENCES pedido (codigo_pedido),
+  FOREIGN KEY (codigo_producto) REFERENCES producto (codigo_producto)
+);
+
+CREATE TABLE pago (
+  codigo_cliente INTEGER NOT NULL,
+  forma_pago VARCHAR(40) NOT NULL,
+  id_transaccion VARCHAR(50) NOT NULL,
+  fecha_pago date NOT NULL,
+  total NUMERIC(15,2) NOT NULL,
+  PRIMARY KEY (codigo_cliente, id_transaccion),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
+
+-- Datos
+INSERT INTO oficina VALUES ('BCN-ES','Barcelona','EspaÃ±a','Barcelona','08019','+34 93 3561182','Avenida Diagonal, 38','3A escalera Derecha');
+INSERT INTO oficina VALUES ('BOS-USA','Boston','EEUU','MA','02108','+1 215 837 0825','1550 Court Place','Suite 102');
+INSERT INTO oficina VALUES ('LON-UK','Londres','Inglaterra','EMEA','EC2N 1HN','+44 20 78772041','52 Old Broad Street','Ground Floor');
+INSERT INTO oficina VALUES ('MAD-ES','Madrid','EspaÃ±a','Madrid','28032','+34 91 7514487','Bulevar Indalecio Prieto, 32','');
+INSERT INTO oficina VALUES ('PAR-FR','Paris','Francia','EMEA','75017','+33 14 723 4404','29 Rue Jouffroy d''abbans','');
+INSERT INTO oficina VALUES ('SFC-USA','San Francisco','EEUU','CA','94080','+1 650 219 4782','100 Market Street','Suite 300');
+INSERT INTO oficina VALUES ('SYD-AU')
